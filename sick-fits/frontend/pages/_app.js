244e439e2f89c46033dd1fde58e6ad75d4a0a6ba
Steven Castroverde <stevencastroverde@steven.local>
2019-04-03 23:19:39 -0600

    added apollo client and querying for items and displaying them on home page. Next working on form to upload data

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,15 +1,27 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import  { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
-
+static async getInitialProps({ Component, ctx}) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+    }
+    //this exposes query to user
+    pageProps.query = ctx.query;
+    return { pageProps };
+}
 render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     return (
         <Container>
-           <Page>
-               <Component></Component>
-            </Page>
+            <ApolloProvider client={apollo}>
+            <Page>
+                <Component {...pageProps}></Component>
+                </Page>
+            </ApolloProvider>
         </Container>
     )    
 }
@@ -17,4 +29,4 @@ render() {
 
 }
 
-export default MyApp;
+export default withData(MyApp);
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,8 @@
-import Link from 'next/link';
+import Items from '../components/Items';
 const Home = props => {
     return (
         <div>
-              <h1>hey</h1>
-              <Link href="/sell">
-                <a>Sell</a>
-              </Link>
+          <Items></Items>
         </div> 
   
     )
