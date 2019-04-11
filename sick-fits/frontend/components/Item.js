4a17347046bcd678746fdb1aa2f456048445b088
Steven Castroverde <stevencastroverde@steven.local>
2019-04-10 22:43:26 -0600

    deleting items

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem';
 import formatMoney from '../lib/formatMoney';
 
 export default class componentName extends Component {
@@ -35,7 +36,7 @@ export default class componentName extends Component {
                 <a>Edit ✏️</a>
             </Link>
             <button>Add to Cart</button>
-            <button>Delete</button>
+            <DeleteItem id={id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     )
