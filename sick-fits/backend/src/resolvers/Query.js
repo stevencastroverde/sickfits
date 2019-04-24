2825bc83ffaef48f4bdf936ce27eb25558e307b3
Steven Castroverde <stevencastroverde@steven.local>
2019-04-23 22:29:39 -0600

    working on signin page

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -6,7 +6,18 @@ const Query = {
 //     }
 items: forwardTo('db'),
 item: forwardTo('db'),
-itemsConnection: forwardTo('db')
+itemsConnection: forwardTo('db'),
+me(parent, args, ctx, info) {
+    //check if current user
+    if ( ! ctx.request.userId) {
+        return null;
+    }
+    return ctx.db.query.user({
+        where: {
+            id: ctx.request.userId
+        }
+    }, info);
+}
 };
 
 module.exports = Query;
