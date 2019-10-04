75fea33726e878fc600da62d0c0588020514a45b
Steven Castroverde <stevencastroverde@steven.local>
2019-10-03 23:20:24 -0600

    moved app to docker container

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+
+const CURRENT_USER_QUERY = gql`
+    query {
+        me {
+            id
+            email
+            name
+            permissions
+        }
+    }
+`;
+const User =  (props) => {
+  return (
+    <Query {...props} query={CURRENT_USER_QUERY}>
+        {payload => console.log(payload) || props.children.payload}
+    </Query>
+  )
+}
+
+User.PropTypes = {
+    children: PropTypes.func.isRequired,
+};
+
+export default User;
+export {CURRENT_USER_QUERY}
+
+
